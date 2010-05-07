25d54650686a82a8170b8b566d62769109b53e3a
Debian Live user <user@debian.(none)>
2010-05-07 18:01:09 +0000

    textstat starting

--- scalasrc/Test2.scala
+++ scalasrc/Test2.scala
@@ -4,9 +4,9 @@
  */
 
 object Test2 extends Application{
-  var x : AnyRef
+  //var x : AnyRef
   val l = List(1,2)
-  l.elements.foreach(
-    System.out.println(20*_)
+  l.foreach( x =>
+    System.out.println(20*x)
   )
 }
---
+++ scalasrc/textstat/App.scala
@@ -0,0 +1,13 @@
+package textstat
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: user
+ * Date: May 7, 2010
+ * Time: 3:43:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object App extends Application{
+  println( "123" )
+}
