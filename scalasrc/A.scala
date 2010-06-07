6609c90f6606e4b3251821896fe345141806299b
denny <denny@ubuntu.(none)>
2010-06-07 12:46:34 +0000

    list to map

--- scalasrc/A.scala
+++ scalasrc/A.scala
@@ -1,5 +1,7 @@
 package scala.p1
 
+import scala.collection._
+
 /**
  * Created by IntelliJ IDEA.
  * User: user
@@ -11,10 +13,29 @@ package scala.p1
 import java.util.{Date, Locale}
 import java.text.DateFormat
 import java.text.DateFormat._
+import collection.immutable.HashMap
+
 object A {
   def main(args : Array[String]):Unit={
-    val now = new Date
-    val df = getDateInstance(LONG, Locale.FRANCE)
-    println(df format now)
+//    val now = new Date
+//    val df = getDateInstance(LONG, Locale.FRANCE)
+//    println(df format now)
+    val l = List(1,2,3)
+    val m = Map() ++ (l map (o => (o,o*o)))
+    println(m+ " "+m.getClass)
+    val m1 = (new mutable.HashMap[Int,Int] /: l){ case (h,i) => h.put(i, i*i); h }
+    println(m1)
+
+   // val CSB = new C{ type T=StringBuilder }
+   // println( CSB.getClass+" "+CSB.newInst )
   }
+ /*
+  abstract class C{
+    type T <: AnyRef
+    def newInst = {
+      //val c = classOf[T]
+      T.newInstance
+    }
+  }
+  */
 }
