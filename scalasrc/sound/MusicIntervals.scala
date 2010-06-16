b206ceb3f1c7a2d3ad27840589cda23cee43b312
denny <denny@ubuntu.(none)>
2010-06-16 17:16:46 +0000

    music intervals

---
+++ scalasrc/sound/MusicIntervals.scala
@@ -0,0 +1,11 @@
+package sound
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: denny
+ * Date: Jun 15, 2010
+ * Time: 3:46:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MusicIntervals
---
+++ scalasrc/sound/Rational.scala
@@ -0,0 +1,11 @@
+package sound
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: denny
+ * Date: Jun 15, 2010
+ * Time: 4:15:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Rational
